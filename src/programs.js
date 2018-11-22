492da8ad69a1cf343c7f8ec3e80d0e04a4b40522
Peter West <peter.g.west@gmail.com>
2018-11-22 01:02:01 +0000

    new app sorting algorithm

--- src/programs.js
+++ src/programs.js
@@ -4,39 +4,44 @@ const fs = require('fs');
 const path = require('path');
 const preferences = require('./preferences');
 
-const APPLICATIONS = [
+const APP_DIRS = [
 	'/usr/share/applications'
 ];
 
 const props = ['Name', 'Comment', 'GenericName', 'Exec', 'Icon'];
 
 const getPrograms = () => {
-	let programs = [];
+	let apps = [];
 
-	APPLICATIONS.forEach(dir => {
+	APP_DIRS.forEach(dir => {
 		fs.readdirSync(dir).map(file => {
 			let id = path.join(dir, file),
 				desktop = fs.readFileSync(id, 'utf8'),
-				program = { id };
+				app = { id };
 
 			desktop.trim().split('\n').forEach(line => {
 				line = line.trim();
 				let kv = line.split('=');
-				if (props.includes(kv[0]) && !program[kv[0]]) {
-					program[kv[0]] = kv[1];
+				if (props.includes(kv[0]) && !app[kv[0]]) {
+					app[kv[0]] = kv[1];
 				}
 			});
-			if (!program.Exec) { return; }
+			if (!app.Exec) { return; }
 
-			program.Exec = program.Exec.replace(/\s%\w/g, ''); // remove parameters
-			program.querysearch = (program.Name || '').toLowerCase() + ':' + (program.Comment || '').toLowerCase() + ':' + (program.GenericName || '').toLowerCase();
-			program.count = preferences.count[id] || 0;
+			app.Exec = app.Exec.replace(/\s%\w/g, ''); // remove parameters
+			app.count = preferences.count[id] || 0;
+
+			app.keywords = [ // array of [keyword, weight]. weight is used to order results
+				...(app.Name || '').toLowerCase().split(/\s+/).map(k => [k, 1000]),
+				...(app.Comment || '').toLowerCase().split(/\s+/).map(k => [k, 1]),
+				...(app.GenericName || '').toLowerCase().split(/\s+/).map(k => [k, 1])
+			].slice(0, 20);
 			
-			programs.push(program);
+			apps.push(app);
 		});
 	});
 
-	return programs.sort((a, b) => b.count - a.count); // most frequently used first
+	return apps.sort((a, b) => b.count - a.count); // most frequently used first
 };
 
 module.exports = getPrograms;
