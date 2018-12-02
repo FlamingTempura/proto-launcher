8074b20444195e573dab34cb88fc2f17dba3b0ea
Peter West <peter.g.west@gmail.com>
2018-12-02 14:02:45 +0000

    themes

--- launcher.cpp
+++ launcher.cpp
@@ -19,6 +19,7 @@ namespace fs = std::filesystem;
 using std::string, std::map, std::vector, std::ifstream, std::ofstream, std::stringstream, std::thread, std::promise;
 
 enum StyleAttribute {
+	NAME,
 	C_TITLE, C_COMMENT, C_BG, C_HIGHLIGHT, C_MATCH, // colors
 	F_REGULAR, F_BOLD, F_SMALLREGULAR, F_SMALLBOLD, F_LARGE // fonts
 };
@@ -64,7 +65,7 @@ map<StyleAttribute, const string> STYLE_ATTRIBUTES = {
 	{ F_LARGE, "large" }
 };
 
-map<StyleAttribute, string> DEFAULT_STYLE = {
+map<StyleAttribute, string> STYLE_DEFAULTS = {
 	{ C_TITLE, "#111111" },
 	{ C_COMMENT, "#999999" },
 	{ C_BG, "#ffffff" },
@@ -76,6 +77,98 @@ map<StyleAttribute, string> DEFAULT_STYLE = {
 	{ F_SMALLBOLD, "Ubuntu,sans-10:bold" },
 	{ F_LARGE, "Ubuntu,sans-20:light" }
 };
+
+vector<map<StyleAttribute, string>> THEMES = { // generated from http://daylerees.github.io/
+	{ { NAME, "Default "} }, // Default
+	{ { NAME, "Mellow Contrast" },     { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#7a7267" }, { C_BG, "#0b0a09" }, { C_HIGHLIGHT, "#13110f" }, { C_MATCH, "#1f8181" } },
+	{ { NAME, "Legacy" },              { C_TITLE, "#aec2e0" }, { C_COMMENT, "#324357" }, { C_BG, "#14191f" }, { C_HIGHLIGHT, "#1b232c" }, { C_MATCH, "#ffffff" } },
+	{ { NAME, "Slime Contrast" },      { C_TITLE, "#ffffff" }, { C_COMMENT, "#4f5a63" }, { C_BG, "#0b0c0d" }, { C_HIGHLIGHT, "#131516" }, { C_MATCH, "#6a9ec5" } },
+	{ { NAME, "Peacock" },             { C_TITLE, "#ede0ce" }, { C_COMMENT, "#7a7267" }, { C_BG, "#2b2a27" }, { C_HIGHLIGHT, "#403c37" }, { C_MATCH, "#ff5d38" } },
+	{ { NAME, "Earthsong Light" },     { C_TITLE, "#4d463e" }, { C_COMMENT, "#d6cab9" }, { C_BG, "#ffffff" }, { C_HIGHLIGHT, "#eeeeee" }, { C_MATCH, "#db784d" } },
+	{ { NAME, "Zacks Contrast" },      { C_TITLE, "#f0f0f0" }, { C_COMMENT, "#777777" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#ff6a38" } },
+	{ { NAME, "Laravel Contrast" },    { C_TITLE, "#ffffff" }, { C_COMMENT, "#615953" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#fc6b0a" } },
+	{ { NAME, "Glowfish Contrast" },   { C_TITLE, "#6ea240" }, { C_COMMENT, "#3c4e2d" }, { C_BG, "#090b07" }, { C_HIGHLIGHT, "#12160d" }, { C_MATCH, "#db784d" } },
+	{ { NAME, "Halflife" },            { C_TITLE, "#cccccc" }, { C_COMMENT, "#555555" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#282828" }, { C_MATCH, "#7d8991" } },
+	{ { NAME, "Mud" },                 { C_TITLE, "#ffffff" }, { C_COMMENT, "#c3b8b7" }, { C_BG, "#403635" }, { C_HIGHLIGHT, "#322a29" }, { C_MATCH, "#ff9787" } },
+	{ { NAME, "Keen" },                { C_TITLE, "#cccccc" }, { C_COMMENT, "#374c60" }, { C_BG, "#111111" }, { C_HIGHLIGHT, "#222222" }, { C_MATCH, "#8767b7" } },
+	{ { NAME, "Patriot Contrast" },    { C_TITLE, "#cad9e3" }, { C_COMMENT, "#515e66" }, { C_BG, "#0d0e0f" }, { C_HIGHLIGHT, "#191b1c" }, { C_MATCH, "#2e6fd9" } },
+	{ { NAME, "Glowfish" },            { C_TITLE, "#6ea240" }, { C_COMMENT, "#3c4e2d" }, { C_BG, "#191f13" }, { C_HIGHLIGHT, "#222a1a" }, { C_MATCH, "#db784d" } },
+	{ { NAME, "Tribal" },              { C_TITLE, "#ffffff" }, { C_COMMENT, "#4a4a54" }, { C_BG, "#19191d" }, { C_HIGHLIGHT, "#33333c" }, { C_MATCH, "#5f5582" } },
+	{ { NAME, "Peel" },                { C_TITLE, "#edebe6" }, { C_COMMENT, "#585146" }, { C_BG, "#23201c" }, { C_HIGHLIGHT, "#403c37" }, { C_MATCH, "#d3643b" } },
+	{ { NAME, "FreshCut Contrast" },   { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#737b84" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#00a8c6" } },
+	{ { NAME, "Earthsong Contrast" },  { C_TITLE, "#ebd1b7" }, { C_COMMENT, "#7a7267" }, { C_BG, "#12100f" }, { C_HIGHLIGHT, "#282521" }, { C_MATCH, "#db784d" } },
+	{ { NAME, "Gloom" },               { C_TITLE, "#d8ebe5" }, { C_COMMENT, "#4f6e64" }, { C_BG, "#2a332b" }, { C_HIGHLIGHT, "#3c4d3e" }, { C_MATCH, "#ff5d38" } },
+	{ { NAME, "Yule" },                { C_TITLE, "#ede0ce" }, { C_COMMENT, "#7a7267" }, { C_BG, "#2b2a27" }, { C_HIGHLIGHT, "#52504b" }, { C_MATCH, "#d63131" } },
+	{ { NAME, "Darkside Contrast" },   { C_TITLE, "#bababa" }, { C_COMMENT, "#494b4d" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#e8341c" } },
+	{ { NAME, "Mintchoc" },            { C_TITLE, "#bababa" }, { C_COMMENT, "#564439" }, { C_BG, "#2b221c" }, { C_HIGHLIGHT, "#3f322a" }, { C_MATCH, "#008d62" } },
+	{ { NAME, "Potpourri" },           { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#696363" }, { C_BG, "#2e2b2c" }, { C_HIGHLIGHT, "#403c37" }, { C_MATCH, "#ed1153" } },
+	{ { NAME, "Juicy" },               { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#777777" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#282828" }, { C_MATCH, "#3bc7b8" } },
+	{ { NAME, "Snappy Light" },        { C_TITLE, "#555555" }, { C_COMMENT, "#bbbbbb" }, { C_BG, "#ffffff" }, { C_HIGHLIGHT, "#eeeeee" }, { C_MATCH, "#f66153" } },
+	{ { NAME, "Goldfish Contrast" },   { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#505c63" }, { C_BG, "#0c0d0e" }, { C_HIGHLIGHT, "#16181a" }, { C_MATCH, "#fa6900" } },
+	{ { NAME, "Kiwi" },                { C_TITLE, "#edebe6" }, { C_COMMENT, "#354341" }, { C_BG, "#161a19" }, { C_HIGHLIGHT, "#282f2d" }, { C_MATCH, "#95c72a" } },
+	{ { NAME, "Spearmint" },           { C_TITLE, "#719692" }, { C_COMMENT, "#93c7c0" }, { C_BG, "#e1f0ee" }, { C_HIGHLIGHT, "#ceebe7" }, { C_MATCH, "#25808a" } },
+	{ { NAME, "Mud Contrast" },        { C_TITLE, "#ffffff" }, { C_COMMENT, "#524343" }, { C_BG, "#0d0b0b" }, { C_HIGHLIGHT, "#171413" }, { C_MATCH, "#f55239" } },
+	{ { NAME, "Earthsong" },           { C_TITLE, "#ebd1b7" }, { C_COMMENT, "#7a7267" }, { C_BG, "#36312c" }, { C_HIGHLIGHT, "#45403b" }, { C_MATCH, "#db784d" } },
+	{ { NAME, "Revelation Contrast" }, { C_TITLE, "#dedede" }, { C_COMMENT, "#7b726b" }, { C_BG, "#0c0b0b" }, { C_HIGHLIGHT, "#1a1918" }, { C_MATCH, "#617fa0" } },
+	{ { NAME, "Github" },              { C_TITLE, "#555555" }, { C_COMMENT, "#b8b6b1" }, { C_BG, "#ffffff" }, { C_HIGHLIGHT, "#eeeeee" }, { C_MATCH, "#008080" } },
+	{ { NAME, "Turnip Contrast" },     { C_TITLE, "#ede0ce" }, { C_COMMENT, "#7a7267" }, { C_BG, "#080809" }, { C_HIGHLIGHT, "#1c1d1f" }, { C_MATCH, "#487d76" } },
+	{ { NAME, "Iceberg" },             { C_TITLE, "#bdd6db" }, { C_COMMENT, "#537178" }, { C_BG, "#323b3d" }, { C_HIGHLIGHT, "#3e4c4f" }, { C_MATCH, "#2d8da1" } },
+	{ { NAME, "Zacks" },               { C_TITLE, "#f0f0f0" }, { C_COMMENT, "#777777" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#333333" }, { C_MATCH, "#ff6a38" } },
+	{ { NAME, "Snappy" },              { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#696969" }, { C_BG, "#393939" }, { C_HIGHLIGHT, "#282828" }, { C_MATCH, "#f66153" } },
+	{ { NAME, "Slate" },               { C_TITLE, "#ebebf4" }, { C_COMMENT, "#515166" }, { C_BG, "#19191f" }, { C_HIGHLIGHT, "#2a2a33" }, { C_MATCH, "#89a7b1" } },
+	{ { NAME, "Shrek" },               { C_TITLE, "#ffffff" }, { C_COMMENT, "#555555" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#333333" }, { C_MATCH, "#857a5e" } },
+	{ { NAME, "Patriot" },             { C_TITLE, "#cad9e3" }, { C_COMMENT, "#515e66" }, { C_BG, "#2d3133" }, { C_HIGHLIGHT, "#40484d" }, { C_MATCH, "#2e6fd9" } },
+	{ { NAME, "Chocolate" },           { C_TITLE, "#ffffff" }, { C_COMMENT, "#795431" }, { C_BG, "#150f08" }, { C_HIGHLIGHT, "#362715" }, { C_MATCH, "#ccb697" } },
+	{ { NAME, "Peacock Contrast" },    { C_TITLE, "#ffffff" }, { C_COMMENT, "#555555" }, { C_BG, "#0c0c0b" }, { C_HIGHLIGHT, "#151513" }, { C_MATCH, "#ff5d38" } },
+	{ { NAME, "Super" },               { C_TITLE, "#ffffff" }, { C_COMMENT, "#465360" }, { C_BG, "#15191d" }, { C_HIGHLIGHT, "#242b32" }, { C_MATCH, "#d60257" } },
+	{ { NAME, "Peacocks In Space" },   { C_TITLE, "#dee3ec" }, { C_COMMENT, "#6e7a94" }, { C_BG, "#2b303b" }, { C_HIGHLIGHT, "#272b34" }, { C_MATCH, "#ff5d38" } },
+	{ { NAME, "Lavender Contrast" },   { C_TITLE, "#e0ceed" }, { C_COMMENT, "#614e6e" }, { C_BG, "#080709" }, { C_HIGHLIGHT, "#110e13" }, { C_MATCH, "#b657ff" } },
+	{ { NAME, "Arstotzka" },           { C_TITLE, "#edebe6" }, { C_COMMENT, "#3f3a36" }, { C_BG, "#211f1e" }, { C_HIGHLIGHT, "#292725" }, { C_MATCH, "#516b6b" } },
+	{ { NAME, "Slime" },               { C_TITLE, "#ffffff" }, { C_COMMENT, "#4f5a63" }, { C_BG, "#292d30" }, { C_HIGHLIGHT, "#384147" }, { C_MATCH, "#9fb3c2" } },
+	{ { NAME, "Sourlick Contrast" },   { C_TITLE, "#dedede" }, { C_COMMENT, "#615953" }, { C_BG, "#060606" }, { C_HIGHLIGHT, "#171615" }, { C_MATCH, "#8ac27a" } },
+	{ { NAME, "Rainbow" },             { C_TITLE, "#c7d0d9" }, { C_COMMENT, "#424c55" }, { C_BG, "#16181a" }, { C_HIGHLIGHT, "#403c37" }, { C_MATCH, "#ef746f" } },
+	{ { NAME, "Goldfish" },            { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#505c63" }, { C_BG, "#2e3336" }, { C_HIGHLIGHT, "#465459" }, { C_MATCH, "#fa6900" } },
+	{ { NAME, "Snappy Contrast" },     { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#696969" }, { C_BG, "#0c0c0c" }, { C_HIGHLIGHT, "#181818" }, { C_MATCH, "#f66153" } },
+	{ { NAME, "Revelation" },          { C_TITLE, "#dedede" }, { C_COMMENT, "#7b726b" }, { C_BG, "#2e2c2b" }, { C_HIGHLIGHT, "#3b3633" }, { C_MATCH, "#617fa0" } },
+	{ { NAME, "Frontier Contrast" },   { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#7a7267" }, { C_BG, "#110f0e" }, { C_HIGHLIGHT, "#221f1c" }, { C_MATCH, "#f23a3a" } },
+	{ { NAME, "Stark Contrast" },      { C_TITLE, "#dedede" }, { C_COMMENT, "#615953" }, { C_BG, "#0b0a0a" }, { C_HIGHLIGHT, "#181716" }, { C_MATCH, "#f03113" } },
+	{ { NAME, "Darkside" },            { C_TITLE, "#bababa" }, { C_COMMENT, "#494b4d" }, { C_BG, "#222324" }, { C_HIGHLIGHT, "#303333" }, { C_MATCH, "#e8341c" } },
+	{ { NAME, "Solarflare Contrast" }, { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#777777" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#ff4e50" } },
+	{ { NAME, "Frontier" },            { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#7a7267" }, { C_BG, "#36312c" }, { C_HIGHLIGHT, "#45403b" }, { C_MATCH, "#f23a3a" } },
+	{ { NAME, "Tonic" },               { C_TITLE, "#eeeeee" }, { C_COMMENT, "#4a5356" }, { C_BG, "#2a2f31" }, { C_HIGHLIGHT, "#353b3e" }, { C_MATCH, "#b8cd44" } },
+	{ { NAME, "Pastel" },              { C_TITLE, "#eeeeee" }, { C_COMMENT, "#444444" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#333333" }, { C_MATCH, "#04c4a5" } },
+	{ { NAME, "Potpourri Contrast" },  { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#696363" }, { C_BG, "#0d0c0c" }, { C_HIGHLIGHT, "#141313" }, { C_MATCH, "#ed1153" } },
+	{ { NAME, "Tron" },                { C_TITLE, "#aec2e0" }, { C_COMMENT, "#324357" }, { C_BG, "#14191f" }, { C_HIGHLIGHT, "#1b232c" }, { C_MATCH, "#ffffff" } },
+	{ { NAME, "Grunge" },              { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#5c634f" }, { C_BG, "#31332c" }, { C_HIGHLIGHT, "#41453a" }, { C_MATCH, "#f56991" } },
+	{ { NAME, "Bold" },                { C_TITLE, "#ffffff" }, { C_COMMENT, "#534b4b" }, { C_BG, "#2a2626" }, { C_HIGHLIGHT, "#393434" }, { C_MATCH, "#f0624b" } },
+	{ { NAME, "Keen Contrast" },       { C_TITLE, "#ffffff" }, { C_COMMENT, "#374c60" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#8767b7" } },
+	{ { NAME, "Crisp" },               { C_TITLE, "#ffffff" }, { C_COMMENT, "#574457" }, { C_BG, "#221a22" }, { C_HIGHLIGHT, "#1c151c" }, { C_MATCH, "#765478" } },
+	{ { NAME, "Otakon" },              { C_TITLE, "#f9f3f9" }, { C_COMMENT, "#515166" }, { C_BG, "#171417" }, { C_HIGHLIGHT, "#332d33" }, { C_MATCH, "#f6e6eb" } },
+	{ { NAME, "Carbonight" },          { C_TITLE, "#b0b0b0" }, { C_COMMENT, "#423f3d" }, { C_BG, "#2e2c2b" }, { C_HIGHLIGHT, "#3b3633" }, { C_MATCH, "#8c8c8c" } },
+	{ { NAME, "FreshCut" },            { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#737b84" }, { C_BG, "#2f3030" }, { C_HIGHLIGHT, "#383939" }, { C_MATCH, "#00a8c6" } },
+	{ { NAME, "Juicy Contrast" },      { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#777777" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#3bc7b8" } },
+	{ { NAME, "Box UK" },              { C_TITLE, "#414f5c" }, { C_COMMENT, "#b8b6b1" }, { C_BG, "#ffffff" }, { C_HIGHLIGHT, "#eeeeee" }, { C_MATCH, "#019d76" } },
+	{ { NAME, "Grunge Contrast" },     { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#5c634f" }, { C_BG, "#0c0c0a" }, { C_HIGHLIGHT, "#1b1c18" }, { C_MATCH, "#f56991" } },
+	{ { NAME, "Mellow" },              { C_TITLE, "#f8f8f2" }, { C_COMMENT, "#7a7267" }, { C_BG, "#36312c" }, { C_HIGHLIGHT, "#45403b" }, { C_MATCH, "#1f8181" } },
+	{ { NAME, "Halflife Contrast" },   { C_TITLE, "#cccccc" }, { C_COMMENT, "#555555" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#7d8991" } },
+	{ { NAME, "Hyrule Contrast" },     { C_TITLE, "#c0d5c1" }, { C_COMMENT, "#716d6a" }, { C_BG, "#0c0c0c" }, { C_HIGHLIGHT, "#141413" }, { C_MATCH, "#569e16" } },
+	{ { NAME, "Iceberg Contrast" },    { C_TITLE, "#bdd6db" }, { C_COMMENT, "#537178" }, { C_BG, "#0b0e0e" }, { C_HIGHLIGHT, "#141718" }, { C_MATCH, "#2d8da1" } },
+	{ { NAME, "Piggy" },               { C_TITLE, "#edebe6" }, { C_COMMENT, "#3f3236" }, { C_BG, "#1c1618" }, { C_HIGHLIGHT, "#34282c" }, { C_MATCH, "#fd6a5d" } },
+	{ { NAME, "Hyrule" },              { C_TITLE, "#c0d5c1" }, { C_COMMENT, "#716d6a" }, { C_BG, "#2d2c2b" }, { C_HIGHLIGHT, "#3d3934" }, { C_MATCH, "#569e16" } },
+	{ { NAME, "Sourlick" },            { C_TITLE, "#dedede" }, { C_COMMENT, "#615953" }, { C_BG, "#2e2c2b" }, { C_HIGHLIGHT, "#3b3633" }, { C_MATCH, "#8ac27a" } },
+	{ { NAME, "Laravel" },             { C_TITLE, "#dedede" }, { C_COMMENT, "#615953" }, { C_BG, "#2e2c2b" }, { C_HIGHLIGHT, "#3b3633" }, { C_MATCH, "#fc6b0a" } },
+	{ { NAME, "Azure" },               { C_TITLE, "#ffffff" }, { C_COMMENT, "#414d62" }, { C_BG, "#181d26" }, { C_HIGHLIGHT, "#33333c" }, { C_MATCH, "#52708b" } },
+	{ { NAME, "Tron Contrast" },       { C_TITLE, "#aec2e0" }, { C_COMMENT, "#324357" }, { C_BG, "#07090b" }, { C_HIGHLIGHT, "#101419" }, { C_MATCH, "#ffffff" } },
+	{ { NAME, "Stark" },               { C_TITLE, "#dedede" }, { C_COMMENT, "#615953" }, { C_BG, "#2e2c2b" }, { C_HIGHLIGHT, "#3b3633" }, { C_MATCH, "#f03113" } },
+	{ { NAME, "Userscape" },           { C_TITLE, "#879bb0" }, { C_COMMENT, "#bbbbbb" }, { C_BG, "#f5f8fc" }, { C_HIGHLIGHT, "#eeeeee" }, { C_MATCH, "#355b8c" } },
+	{ { NAME, "Carbonight Contrast" }, { C_TITLE, "#ffffff" }, { C_COMMENT, "#423f3d" }, { C_BG, "#000000" }, { C_HIGHLIGHT, "#151515" }, { C_MATCH, "#8c8c8c" } },
+	{ { NAME, "Gloom Contrast" },      { C_TITLE, "#d8ebe5" }, { C_COMMENT, "#4f6e64" }, { C_BG, "#0f120f" }, { C_HIGHLIGHT, "#1b211c" }, { C_MATCH, "#ff5d38" } },
+	{ { NAME, "Lavender" },            { C_TITLE, "#e0ceed" }, { C_COMMENT, "#614e6e" }, { C_BG, "#29222e" }, { C_HIGHLIGHT, "#3a2f42" }, { C_MATCH, "#b657ff" } },
+	{ { NAME, "Solarflare" },          { C_TITLE, "#e3e2e0" }, { C_COMMENT, "#777777" }, { C_BG, "#222222" }, { C_HIGHLIGHT, "#282828" }, { C_MATCH, "#ff4e50" } },
+	{ { NAME, "Turnip" },              { C_TITLE, "#ede0ce" }, { C_COMMENT, "#7a7267" }, { C_BG, "#1a1b1d" }, { C_HIGHLIGHT, "#222222" }, { C_MATCH, "#487d76" } } 
+};
+
+map<StyleAttribute, string> STYLE_OVERRIDE = {};
 
 Display *display;
 int screen;
@@ -89,9 +182,10 @@ int selected = 0;
 int cursor = 0;
 bool cursorVisible = false;
 int width;
+int theme = 0;
+XSetWindowAttributes attributes;
 vector<Application> applications;
 vector<Result> results;
-map<StyleAttribute, string> style = DEFAULT_STYLE;
 map<StyleAttribute, XftFont*> fonts;
 map<StyleAttribute, XftColor> colors;
 
@@ -219,10 +313,19 @@ void readConfig () {
 		if (i == string::npos) { continue; }
 		const string key = line.substr(0, i);
 		const string val = line.substr(i + 1);
-		if (key.find("/") == string::npos) {
+		if (key == "theme") {
+			int j = 0;
+			for (auto &t : THEMES) {
+				if (t[NAME] == val) {
+					theme = j;
+					break;
+				}
+				j++;
+			}
+		} else if (key.find("/") == string::npos) {
 			for (const auto &[type, attr] : STYLE_ATTRIBUTES) {
 				if (key == attr) {
-					style[type] = val;
+					STYLE_OVERRIDE[type] = val;
 					break;
 				}
 			}
@@ -241,9 +344,10 @@ void writeConfig () {
 	ofstream outfile;
 	outfile.open(CONFIG);
 	outfile << "[Style]\n";
+	outfile << "theme=" << THEMES[theme][NAME] << "\n";
 	for (const auto &[type, attr] : STYLE_ATTRIBUTES) {
-		if (style[type] != DEFAULT_STYLE[type]) {
-			outfile << STYLE_ATTRIBUTES[type] << "=" << style[type] << "\n";
+		if (STYLE_OVERRIDE.find(type) != STYLE_OVERRIDE.end()) {
+			outfile << STYLE_ATTRIBUTES[type] << "=" << STYLE_OVERRIDE[type] << "\n";
 		}
 	}
 	outfile << "\n[Launches]\n";
@@ -333,6 +437,35 @@ void launch (Application &app) {
 	exit(0);
 }
 
+Visual *visual;
+Colormap colormap;
+
+void updateStyle () {
+	map<StyleAttribute, string> style = STYLE_DEFAULTS;
+
+	for (const auto &[type, val] : THEMES[theme]) {
+		style[type] = val;
+	}
+
+	for (const auto &[type, val] : STYLE_OVERRIDE) {
+		style[type] = val;
+	}
+
+	for (const StyleAttribute c : COLORS) {
+		unsigned short r = stol(style[c].substr(1, 2), nullptr, 16) * 256;
+		unsigned short g = stol(style[c].substr(3, 2), nullptr, 16) * 256;
+		unsigned short b = stol(style[c].substr(5, 2), nullptr, 16) * 256;
+		XRenderColor xrcolor = { .red = r, .green = g, .blue = b, .alpha = 255 * 256 };
+		XftColorAllocValue(display, visual, colormap, &xrcolor, &colors[c]);
+	}
+
+	for (const StyleAttribute c : FONTS) {
+		fonts[c] = XftFontOpenName(display, screen, style[c].c_str());
+	}
+
+	XSetWindowBackground(display, window, colors[C_BG].pixel);
+}
+
 void onKeyPress (XEvent &event) {
 	char text[128] = {0};
 	KeySym keysym;
@@ -373,6 +506,15 @@ void onKeyPress (XEvent &event) {
 				query.erase(cursor, 1);
 			}
 			break;
+		case XK_F7:
+			if (event.xkey.state == 1) {
+				theme = theme > 0 ? theme - 1 : THEMES.size() - 1;
+			} else {
+				theme = theme < THEMES.size() - 1 ? theme + 1 : 0;
+			}
+			updateStyle();
+			writeConfig();
+			break;
 		default:
 			if (textlength == 1) { // check it's a character
 				query = query.substr(0, cursor) + text + query.substr(cursor, query.length());
@@ -382,35 +524,21 @@ void onKeyPress (XEvent &event) {
 	queryi = lowercase(query);
 }
 
+
 int main () {
 	auto awaitApps = async(getApplications); // prepare list of apps in the background
 	readConfig();
 
 	display = XOpenDisplay(NULL);
 	screen = DefaultScreen(display);
-	Visual *visual = DefaultVisual(display, screen);
-	Colormap colormap = DefaultColormap(display, screen);
+	visual = DefaultVisual(display, screen);
+	colormap = DefaultColormap(display, screen);
 	int depth = DefaultDepth(display, screen);
 	int screenWidth = DisplayWidth(display, screen);
 	width = screenWidth / 3.4;
 	int x = screenWidth / 2 - width / 2;
 	bool applicationsLoaded = false;
-
-	for (const StyleAttribute c : COLORS) {
-		unsigned short r = stol(style[c].substr(1, 2), nullptr, 16) * 256;
-		unsigned short g = stol(style[c].substr(3, 2), nullptr, 16) * 256;
-		unsigned short b = stol(style[c].substr(5, 2), nullptr, 16) * 256;
-		XRenderColor xrcolor = { .red = r, .green = g, .blue = b, .alpha = 255 * 256 };
-		XftColorAllocValue(display, visual, colormap, &xrcolor, &colors[c]);
-	}
-
-	for (const StyleAttribute c : FONTS) {
-		fonts[c] = XftFontOpenName(display, screen, style[c].c_str());
-	}
-
-	XSetWindowAttributes attributes;
-	attributes.background_pixel = colors[C_BG].pixel;
-
+	
 	window = XCreateWindow(display, XRootWindow(display, screen),
 		x, 200, width, INPUT_HEIGHT,
 		5, depth, InputOutput, visual, CWBackPixel, &attributes);
@@ -424,6 +552,8 @@ int main () {
 
 	XGCValues gr_values;
 	gc = XCreateGC(display, window, 0, &gr_values);
+
+	updateStyle();
 
 	setProperty("_NET_WM_WINDOW_TYPE", "_NET_WM_WINDOW_TYPE_DOCK");
 	setProperty("_NET_WM_STATE", "_NET_WM_STATE_ABOVE");
